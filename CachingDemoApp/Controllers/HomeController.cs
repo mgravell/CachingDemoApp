f974d792a28fc06ec888cd096c1293eb36a0b7d9
Marc Gravell <marc.gravell@gmail.com>
2024-07-02 13:32:37 +0100

    code

---
+++ CachingDemoApp/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using CachingDemoApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CachingDemoApp.Controllers
+{
+    public class HomeController(SomeBackendDataService backend) : Controller
+    {
+        public async Task<IActionResult> IndexAsync(CancellationToken cancellation)
+        {
+            ViewData.Add("clock", DateTime.UtcNow);
+            var timer = Stopwatch.StartNew();
+
+            var data = await backend.GetSomeExpensiveDataWithoutCacheAsync(cancellation);
+            //var data = await backend.GetSomeExpensiveDataWithoutCacheAsync(cancellation);
+            //var data = await backend.GetSomeExpensiveDataWithHybridCachingAsync(cancellation);
+
+            timer.Stop();
+            ViewData.Add("timer", timer.ElapsedMilliseconds);
+            return View(data);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CachingDemoApp/Program.cs
@@ -0,0 +1,35 @@
+using CachingDemoApp.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<SomeBackendDataService>();
+builder.Services.AddStackExchangeRedisCache(setup =>
+{
+    setup.Configuration = "127.0.0.1:6379";
+});
+// builder.Services.AddMemoryCache();
+// builder.Services.AddHybridCache();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
